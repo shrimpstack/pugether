6dc1cf34d45bffaebb2afa1d10689c0ceb854dd2
shrimpstack <89066728+shrimpstack@users.noreply.github.com>
2023-12-18 12:26:30 +0800

    測試

--- main/index.js
+++ main/index.js
@@ -1,18 +1,14 @@
-window.Pugether = (() => {
-  function PGT_close() {
+window.pugether_open = function pugether_open() {
+  if(location.origin != "https://www.plurk.com") {
+    alert("只能在噗浪開啟噗給樂");
+    return;
+  }
+  if(!$('#filter_tab').length) {
+    alert("只能在河道開啟噗給樂");
+    return;
+  }
+  window.pugether_close = function () {
     $('#pugether').remove();
-    delete window.Pugether;
+    delete window.pugether_close;
   }
-  static PGT_open() {
-    if(location.origin != "https://www.plurk.com") {
-      alert("只能在噗浪開啟噗給樂");
-      return;
-    }
-    if(!$('#filter_tab').length) {
-      alert("只能在河道開啟噗給樂");
-      return;
-    }
-  }
-  PGT_open();
-  return { close: PGT_close };
-})();
+}
