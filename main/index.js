152a52c0fe576d3dd83ad992607b7468766fe1c2
shrimpstack <89066728+shrimpstack@users.noreply.github.com>
2023-12-18 12:32:58 +0800

    測試

--- main/index.js
+++ main/index.js
@@ -1,4 +1,4 @@
-window.pugether_open = function pugether_open() {
+(() => {
   if(location.origin != "https://www.plurk.com") {
     alert("只能在噗浪開啟噗給樂");
     return;
@@ -7,8 +7,9 @@ window.pugether_open = function pugether_open() {
     alert("只能在河道開啟噗給樂");
     return;
   }
-  window.pugether_close = function () {
+  window.Pugether = { close: PGT_close };
+  function PGT_close() {
     $('#pugether').remove();
-    delete window.pugether_close;
+    delete window.Pugether;
   }
-}
+})()
